79636be4160d3b0fc5d5d64037b9ce4ff827cb3c
cockeater99 <you@example.com>
2023-08-05 04:57:39 +0200

    Base

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use std::{thread, time::Duration};
 
+use chrono::Local;
+use log::LevelFilter;
+use log4rs::{append::{console::ConsoleAppender, file::FileAppender}, encode::pattern::PatternEncoder, Config, config::{Appender, Root}};
 use server::Server;
 
 mod packet;
@@ -11,9 +14,26 @@ mod state;
 mod states;
 mod entity;
 
+fn init_logger()
+{
+    let console = ConsoleAppender::builder().encoder(Box::new(PatternEncoder::default())).build();
+    let logfile = FileAppender::builder()
+    .encoder(Box::new(PatternEncoder::default()))
+    .build(format!("log/{}.log", Local::now()))
+    .unwrap();
+
+    let config = Config::builder()
+    .appender(Appender::builder().build("console", Box::new(console)))
+    .appender(Appender::builder().build("logfile", Box::new(logfile)))
+    .build(Root::builder().appender("console").appender("logfile").build(LevelFilter::Debug))
+    .unwrap();
+    
+    log4rs::init_config(config).unwrap();
+}
+
 fn main() 
 {
-    log4rs::init_file("logging_config.yaml", Default::default()).unwrap();
+    init_logger();
 
     let _server = Server::start("0.0.0.0:7606", "0.0.0.0:8606");
     let dur = Duration::from_secs(2);
